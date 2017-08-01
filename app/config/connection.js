cdd4740e6aca9f273bbe151f7a8e92541b9f3558
Shirlon Banks <sssbanks@gmail.com>
2017-07-31 17:43:34 -0700

    debugging

---
+++ app/config/connection.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+var connection;
+
+// For Heroku Deployment vs. Local MySQL Database
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Passw@rd82!', // Add your password
+    database : 'burgers_db' // Add your database
+  });
+}
+
+
+// Export the Connection
+module.exports = connection;

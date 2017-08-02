030881e6cf5abef43e88a8514271784fdfadf75b
Shirlon Banks <sssbanks@gmail.com>
2017-08-02 10:32:44 -0700

    heroku debug

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override')
 
 var app = express();
+
 //Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + '/public'));
-// app.use(express.static('public'));
 
-// Parse application/x-www-form-urlencoded
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Handlebars
@@ -20,6 +20,6 @@ app.set('view engine', 'handlebars');
 var router = require('./controllers/burgers_controller.js');
 app.use('/', router);
 
-// Open Server
+// Server
 var port = process.env.PORT || 3000;
 app.listen(port);

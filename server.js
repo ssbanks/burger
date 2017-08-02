b2da734525fa5c0858d545650519f06bc3b0bf12
Shirlon Banks <sssbanks@gmail.com>
2017-08-02 12:21:35 -0700

    more debug

--- server.js
+++ server.js
@@ -1,25 +1,60 @@
-//Node Dependencies
+//Dependencies
 var express = require('express');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override')
+var methodOverride = require('method-override');
+var exphbs = require('express-handlebars');
 
+// Sets up the Express App
 var app = express();
+var PORT = process.env.PORT || 3000;
 
-//Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static(process.cwd() + '/public'));
-
-
-app.use(bodyParser.urlencoded({ extended: false }));
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Handlebars
-var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
+//Routes
 var router = require('./controllers/burgers_controller.js');
 app.use('/', router);
+app.use(express.static(__dirname + '/public'));
 
-// Server
-var port = process.env.PORT || 3000;
-app.listen(port);
+
+
+// Server(listening)
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
+// // Server.js - This file is the initial starting point for the Node/Express server.
+// // *********************************************************************************
+
+
+
+// // Sets up the Express app to handle data parsing
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.text());
+// app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// // Override with POST having ?_method=DELETE
+// app.use(methodOverride("_method"));
+
+// // Static directory
+// app.use(express.static("app/public"));
+
+
+// // Routes
+// // =============================================================
+// require("./app/routes/api-routes.js")(app);
+// require("./app/routes/view-routes.js")(app);
+
+// // Starts the server to begin listening
+// // =============================================================
+// app.listen(PORT, function() {
+//   console.log("App listening on PORT " + PORT);
+// });
